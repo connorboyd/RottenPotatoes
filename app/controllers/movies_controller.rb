54349ac8794bd55b57a3c36aaae8593c92ac9a14
Connor Boyd <Connor.Boyd@spir3.com>
2013-10-15 12:55:40 -0600

    Filtering works

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,11 +8,13 @@ class MoviesController < ApplicationController
 
   def index
     # @movies = Movie.all
+    @all_ratings = Movie.ratings
     @order = params[:order]
+    @ratings = (params[:ratings]) ? params[:ratings].values : []
     if Movie.column_names.include? @order
-      @movies = Movie.find(:all, :order => @order)
+      @movies = Movie.find(:all, :order => @order, :conditions => ['rating in (?)', @ratings])
     else
-      @movies = Movie.all
+      @movies = Movie.find(:all, :conditions => ['rating in (?)', @ratings])
     end
   end
 
@@ -42,6 +44,19 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+
+  private
+  def ensure_table_filters
+    # this is awful, gotta find some elegence with merge or something
+    session[:table_filters] = {} if session[:table_filters].nil?
+    if params[:order] != session[:table_filters][:order] || params[:ratings] != session[:table_filters][:ratings]
+      needs_redirect = true
+    end
+    session[:table_filters][:order] = params[:order] unless params[:order].nil?
+    session[:table_filters][:ratings] = params[:ratings] unless params[:ratings].nil?
+
+    redirect_to movies_path(session[:table_filters]) if needs_redirect
   end
 
 end
